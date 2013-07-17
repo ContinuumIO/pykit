08c197417fb011a8d9e8715aaa002426e9a44ce9
Mark Florisson <markflorisson88@gmail.com>
2013-07-17 15:44:03 +0100

    Parametrized test utility

--- pykit/tests/__init__.py
+++ pykit/tests/__init__.py
@@ -34,3 +34,29 @@ class SourceTestCase(unittest.TestCase):
         self.env = environment.fresh_env()
 
         self.eq = self.assertEqual
+
+
+def parametrize(testcase, param, suite=None):
+    """
+    Create a suite containing all tests taken from the given
+    testcase class, passing them the parameter 'param'.
+    """
+    testloader = unittest.TestLoader()
+    testnames = testloader.getTestCaseNames(testcase)
+    suite = suite or unittest.TestSuite()
+    for name in testnames:
+        test = testcase(name)
+        test.setparam(param)
+        suite.addTest(test)
+    return suite
+
+def make_load_tests(*testcases):
+    """
+    Override TestCase creation:
+
+        load_tests = make_load_tests(TestCase1(
+    """
+    def load_tests(loader, standard_tests, pattern):
+        standard_tests.addTests(testcases)
+        return standard_tests
+    return load_tests
