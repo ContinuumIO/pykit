8b74c6c4f4245d0404700dda00a5188b20ee118c
Mark Florisson <markflorisson88@gmail.com>
2013-08-04 22:33:08 +0100

    Add inliner

---
+++ pykit/transform/inline.py
@@ -0,0 +1,89 @@
+# -*- coding: utf-8 -*-
+
+"""
+Function inlining.
+"""
+
+from pykit.error import CompileError
+from pykit.analysis import cfa, defuse, loop_detection
+from pykit.ir import Function, Builder, Undef, findallops, copy_function, replace_uses
+from pykit.ir.verification import verify_op_syntax
+from pykit.transform import ret as ret_normalization
+
+def rewrite_return(func):
+    """Rewrite ret ops to assign to a variable instead, which is returned"""
+    ret_normalization.run(func)
+    [ret] = findallops(func, 'ret')
+    [value] = ret.args
+    ret.delete()
+    return value
+
+def v(func):
+    for op in func.ops:
+        verify_op_syntax(op)
+
+def inline(func, call, uses=None):
+    """
+    Inline the call instruction into func.
+
+    :param uses: defuse information
+    """
+    callee = call.args[0]
+    # assert_inlinable(func, call, callee, uses)
+
+    builder = Builder(func)
+    builder.position_before(call)
+    inline_header, inline_exit = builder.splitblock()
+    new_callee = copy_function(callee, temper=func.temp)
+    result = rewrite_return(new_callee)
+
+    # Fix up arguments
+    uses = defuse.defuse(new_callee)
+    for funcarg, arg in zip(new_callee.args, call.args[1]):
+        replace_uses(funcarg, arg, uses)
+
+    # Copy blocks
+    after = inline_header
+    for block in new_callee.blocks:
+        block.parent = None
+        func.add_block(block, after=after)
+        after = block
+
+    # Fix up wiring
+    builder.jump(new_callee.startblock)
+    with builder.at_end(new_callee.exitblock):
+        builder.jump(inline_exit)
+
+    # Fix up final result of call
+    if result is not None:
+        # non-void return
+        result.unlink()
+        result.result = call.result
+        call.replace(result)
+    else:
+        call.delete()
+
+def assert_inlinable(func, call, callee, uses):
+    """
+    Verify that a function call can be inlined.
+
+    We can inline generators if they are consumed in a single loop:
+
+        - iter(g) must be in a loop header
+        - next(g) must be in the loop body
+
+    :return: None if inlineable, or an exception with a message
+    """
+    if not isinstance(callee, Function):
+        return CompileError("Cannot inline external function: %s" % (callee,))
+
+    yields = findallops(callee, 'yield')
+    if yields:
+        for use in uses[call]:
+            if use.opcode not in ('iter', 'next'):
+                return CompileError(
+                    "Cannot inline generator with use %s" % (use,))
+
+        if len(uses[call]) != 2:
+            return CompileError("Can only")
+        loops = loop_detection.find_natural_loops(func)
---
+++ pykit/transform/tests/test_inlining.py
@@ -0,0 +1,67 @@
+# -*- coding: utf-8 -*-
+from __future__ import print_function, division, absolute_import
+
+import unittest
+
+from pykit.analysis import cfa
+from pykit.parsing import from_c
+from pykit.transform import ret, inline
+from pykit.ir import opcodes, findallops, verify, interp
+
+class TestInlining(unittest.TestCase):
+    def test_inline(self):
+        simple = """
+        #include <pykit_ir.h>
+
+        int callee(int i) {
+            return i * i;
+        }
+
+        int caller(int i) {
+            int x = call(callee, list(i));
+            return x;
+        }
+        """
+        mod = from_c(simple)
+        func = mod.get_function("caller")
+        [callsite] = findallops(func, 'call')
+        inline.inline(func, callsite)
+        cfa.run(func)
+        verify(func)
+        assert interp.run(func, args=[10]) == 100
+        assert len(list(func.blocks)) == 1
+        assert opcodes(func) == ['mul', 'ret']
+
+    def test_inline2(self):
+        harder = """
+        int callee(int i) {
+            (void) print(i);
+            while (i < 10) {
+                i = i + 1;
+                return i * 2;
+            }
+            return i;
+        }
+
+        int caller() {
+            int x = 4;
+            while (x < 10) {
+                (void) print(x);
+                x = call(callee, list(x));
+            }
+            return x;
+        }
+        """
+        mod = from_c(harder)
+        func = mod.get_function("caller")
+        verify(func)
+        result = interp.run(func)
+
+        [callsite] = findallops(func, 'call')
+        inline.inline(func, callsite)
+        cfa.run(func)
+        verify(func)
+
+        # TODO: update phi when splitting blocks
+        # result2 = interp.run(func)
+        # assert result == result2
