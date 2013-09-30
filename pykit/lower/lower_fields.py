cb6438cc2bc13639ff3942f8318c6a2306afe2f4
Mark Florisson <markflorisson88@gmail.com>
2013-09-30 11:49:42 +0100

    Add a pass to lower field access of struct *

--- pykit/lower/lower_fields.py
+++ pykit/lower/lower_fields.py
@@ -1,1 +1,24 @@
-__author__ = 'mark'
+# -*- coding: utf-8 -*-
+
+"""
+Rewrite field accesses on pointers.
+"""
+
+from pykit.ir import Builder
+
+def lower_fields(func, env=None):
+    b = Builder(func)
+
+    for op in func.ops:
+        if op.opcode in ("getfield", "setfield") and op.args[0].type.is_pointer:
+            b.position_before(op)
+            p = op.args[0]
+            load = b.load(p.type.base, [p])
+            args = [load] + op.args[1:]
+            op.set_args(args)
+
+            if op.opcode == "setfield":
+                b.position_after(op)
+                b.store(op, p)
+
+run = lower_fields
